6178e8996f29acc6748d3509ec290490f6ca0d8c
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-19 16:14:43 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -21,7 +21,8 @@ def remove_strawberry(contacts)
       contact_detail_hash.each do |attribute, data|
         #binding.pry
         if attribute == :favorite_ice_cream_flavors
-          binding.pry
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+          
         end
       end
     end
